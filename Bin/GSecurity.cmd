dfac5a59c1bac678f01b6aa37efcff269a7ca11b
Alatnicar <znastidobrostoje@gmail.com>
2025-06-12 07:54:14 +0200

    Add files via upload

---
+++ Bin/GSecurity.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+:: Perms
+icacls "%systemdrive%\Users" /remove "Everyone"
+icacls "%systemdrive%\Users\Public" /inheritance:r
+icacls "%systemdrive%\Users\Public\Desktop" /setowner "console logon" /t
+icacls "%USERPROFILE%\" /setowner "%username%" /t /c /l
+icacls "%USERPROFILE%\" /remove "System" /t /c /l
+icacls "%USERPROFILE%\" /remove "Administrators" /t /c /l
+
+:: Services stop and disable
+sc stop LanmanWorkstation
+sc stop LanmanServer
+sc stop seclogon
+sc config LanmanWorkstation start= disabled
+sc config LanmanServer start= disabled
+sc config seclogon start= disabled
+
+:: Security Policy Import
+LGPO.exe /s GSecurity.inf
